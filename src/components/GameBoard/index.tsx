01a4b60cd05d003fb0fb2e6260df5b06d83fa216
alexandredrossetto <alexandre.rossetto@captalys.com.br>
2022-04-28 22:29:09 -0300

    housekeeping: removing nanoid as its not best practice and adding another method

--- src/components/GameBoard/index.tsx
+++ src/components/GameBoard/index.tsx
@@ -1,12 +1,13 @@
+import React from 'react';
 import { Container } from './styles'
 import { Square } from '../Square'
 import { GameBoardProps } from './interface'
-import { nanoid } from 'nanoid'
 
 export const GameBoard = ({ squares, onClick } : GameBoardProps) => (
   <Container>
-    {squares.map((square : string, i : number) => (
-      <Square idx={i} key={nanoid()} value={square} onClick={() => onClick(i)} />
-    ))}
+    {React.Children.toArray(
+      squares.map((square : string, i : number) => (
+      <Square idx={i} value={square} onClick={() => onClick(i)} />
+    )))}
   </Container>
 )
