ade6d36d0d05bb9e91bb995397cac88595f8f1a7
alexandredrossetto <alexandre.rossetto@captalys.com.br>
2022-04-29 03:02:15 -0300

    housekeeping: fixing spaces between import and interface

--- src/components/GameResultModal/interface.ts
+++ src/components/GameResultModal/interface.ts
@@ -1,5 +1,6 @@
 import { LeaderboardType } from '../Leaderboard/interface';
 import { Players } from '../RegisterNewRoundModal/interface';
+
 export interface GameResultModalProps {
   isOpen: boolean;
   result: LeaderboardType;
